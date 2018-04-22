456f30c93d1167c517b335e706c3e773693dabf5
paosch <paola_schiaffino@yahoo.co.uk>
2018-04-22 12:55:57 +0100

    Airport can instruct plane to land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,11 @@ describe Airport do
       expect(airport.land(plane)).to eq(plane)
     end
   end
+
+  describe '#take_off' do
+    it "allows plane to take off and confirms it's left the airport" do
+      airport.land(plane)
+      expect(airport.take_off(plane)).not_to eq(plane) # confirms plane not there
+    end
+  end
 end
